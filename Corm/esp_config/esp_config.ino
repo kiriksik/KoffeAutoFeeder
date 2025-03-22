daf0fad2ad5f4a1e3c0c68b8e5c04b36d8647e21
Kirill Makeev <132159721+kiriksik@users.noreply.github.com>
2025-03-22 23:28:07 +0300

    V 0.99
    Added servo ability to rotate 0-90, feed handler, cam

--- Corm/esp_config/esp_config.ino
+++ Corm/esp_config/esp_config.ino
@@ -1,18 +1,41 @@
 #include <EEPROM.h>
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
+#include <esp_camera.h>
 #include <ESPAsyncWebServer.h>
 #include <WebSocketsClient.h>
 #include <SocketIOclient.h>
-#include <Servo.h>
+#include <ESP32Servo.h>
 #include <ArduinoJson.h>
+#include <Base64.h>
+
+#define CONFIG_ESP32_SPIRAM_SUPPORT 1
+
+#define PWDN_GPIO_NUM  32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  0
+#define SIOD_GPIO_NUM  26
+#define SIOC_GPIO_NUM  27
+#define Y9_GPIO_NUM    35
+#define Y8_GPIO_NUM    34
+#define Y7_GPIO_NUM    39
+#define Y6_GPIO_NUM    36
+#define Y5_GPIO_NUM    21
+#define Y4_GPIO_NUM    19
+#define Y3_GPIO_NUM    18
+#define Y2_GPIO_NUM    5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM  23
+#define PCLK_GPIO_NUM  22
+#define LED_GPIO_NUM   4
+
 
 // Серво
-Servo servo;
-const int servoPin = 16; // Пин сервопривода (D1)
-const int ledPin = 2; //Пин диода (D?)
+Servo feederServo;
+const int servoPin = 2;  // Пин сервопривода
+const int ledPin = 2;     // Пин встроенного светодиода
 
-// Начальный вайфай
-const char* ssidAP = "AutoFeederSetup";
+// Начальный Wi-Fi
+// const char* ssidAP = "AutoFeederSetup";
 const char* passwordAP = "12345678";
 
 String wifiSSID = "";
@@ -20,24 +43,36 @@ String wifiPassword = "";
 
 AsyncWebServer server(80);
 WebSocketsClient webSocket;
-SocketIOclient  socketIO;
+SocketIOclient socketIO;
 
-unsigned long feedingInterval = 3600000; // в миллисекундах
+unsigned long feedingInterval = 3600000;  // в миллисекундах
+unsigned long servoOpenTime = 1000; //в миллисекундах
 unsigned long lastFeedingTime = 0;
+
 
 const char* serverHost = "192.168.31.172";
 const int serverPort = 5000;
-const char* deviceID = "feeder_001";
+const char* deviceID = "feeder_003";
+const char* password = "123123123";
 
 unsigned long lastPingTime = 0;
-unsigned long pingInterval = 30000; // Интервал пинга
+unsigned long pingInterval = 30000;  // Интервал пинга
+
 
 int trying = 0;
+bool busy = false;
 
 void feedAnimal() {
-  servo.write(90);
-  delay(1000);
-  servo.write(0);
+
+
+  Serial.println("Opening feeder...");
+  feederServo.write(0);  
+  delay(100);
+  feederServo.write(90);
+  delay(servoOpenTime);
+  Serial.println("Closing feeder...");
+  feederServo.write(0); 
+  // feederServo.detach();
 }
 
 void test() {
@@ -46,6 +81,10 @@ void test() {
   digitalWrite(ledPin, LOW);
 }
 
+void sendImage(camera_fb_t* fb) {
+  String base64Image = base64::encode(fb->buf, fb->len);
+  sendImageToServer(base64Image);
+}
 
 void saveWiFiCredentials(const String& ssid, const String& password) {
   EEPROM.begin(512);
@@ -61,6 +100,22 @@ void saveWiFiCredentials(const String& ssid, const String& password) {
 }
 
 
+void sendImageToServer(String imageData) {
+  DynamicJsonDocument doc(1024);
+  JsonArray array = doc.to<JsonArray>();
+  array.add("camera_frame");
+  JsonObject param = array.createNestedObject();
+  param["image"] = imageData;
+  param["device_id"] = deviceID;
+
+  String output;
+  serializeJson(doc, output);
+  socketIO.sendEVENT(output);
+  Serial.println("[Socket.IO] Image sent to server.");
+  doc.clear();
+}
+
+
 void loadWiFiCredentials(String& ssid, String& password) {
   EEPROM.begin(512);
   char ssidBuff[32];
@@ -73,107 +128,85 @@ void loadWiFiCredentials(String& ssid, String& password) {
   password = String(passBuff);
 }
 
+void socketIOEvent(socketIOmessageType_t type, uint8_t* payload, size_t length) {
+  switch (type) {
+    case sIOtype_DISCONNECT:
+      Serial.println("[Socket.IO] Disconnected!");
+      break;
+    case sIOtype_CONNECT:
+      {
+        socketIO.send(sIOtype_CONNECT, "/");
+        Serial.println("[Socket.IO] Connected!");
 
-void socketIOEvent(socketIOmessageType_t type, uint8_t * payload, size_t length) {
-    switch(type) {
-        case sIOtype_DISCONNECT:
-          Serial.println("[Socket.IO] Disconnected!");
-          break;
-        case sIOtype_CONNECT:
-        {
-          socketIO.send(sIOtype_CONNECT, "/");
+        DynamicJsonDocument doc(1024);
+        JsonArray array = doc.to<JsonArray>();
+        array.add("identify");
+        JsonObject param = array.createNestedObject();
+        param["device_id"] = deviceID;
+        param["type"] = "device";
+        param["password"] = password;
+        param["feeding_interval"] = feedingInterval;
+        param["url_to_cam"] = "someurl";
 
-          Serial.println("[Socket.IO] Connected!");
-          DynamicJsonDocument doc(1024);
-          JsonArray array = doc.to<JsonArray>();
-          array.add("identify");
-          JsonObject param = array.createNestedObject();
-          param["device_id"] = deviceID;
-          param["feeding_interval"] = feedingInterval;
-          param["url_to_cam"] = "someurl";
-
-          String output;
-          serializeJson(doc, output);
-
-          socketIO.sendEVENT(output);
-          break;
+        String output;
+        serializeJson(doc, output);
+        socketIO.sendEVENT(output);
+        break;
+      }
+    case sIOtype_EVENT:
+      {
+        String data;
+        for (size_t i = 0; i < length; i++) {
+          data += (char)payload[i];
         }
-        case sIOtype_EVENT: {
-          Serial.println("[SocketIO] Received event:");
+        Serial.println("[SocketIO] Received event: " + data);
 
-          String data;
-          for (size_t i = 0; i < length; i++) {
-              // trying += 1;
-              data += (char)payload[i];
-          }
-          Serial.println(data);
-
-          DynamicJsonDocument doc(1024);
-          DeserializationError error = deserializeJson(doc, data);
-          if (error) {
-              trying += 10;
-              Serial.print(F("JSON Parsing failed: "));
-              Serial.println(error.c_str());
-              return;
-          }
-
-          JsonObject jsonObject = doc[1].as<JsonObject>();
-          String command = jsonObject["action"];
-    
-          if (command == "feed") {
-              Serial.println("Команда: кормление!");
-              trying += 1;
-          } else if (command == "set_interval") {
-            int interval = jsonObject["interval"];
-            feedingInterval = interval * 60000;
-          } else {
-              Serial.println("Неизвестная команда: " + command);
-          }
-          DynamicJsonDocument doc2(1024);
-          JsonArray array2 = doc2.to<JsonArray>();
-          array2.add("smth");
-          JsonObject param = array2.createNestedObject();
-          param["info"] = command;
-
-          String output2;
-          serializeJson(doc2, output2);
-
-          // Отправляем событие
-          socketIO.sendEVENT(output2);
-          break;
+        DynamicJsonDocument doc(1024);
+        DeserializationError error = deserializeJson(doc, data);
+        if (error) {
+          Serial.print("JSON Parsing failed: ");
+          Serial.println(error.c_str());
+          return;
         }
-        case sIOtype_ACK:
-        {
-          // Serial.println("[IOc] get ack: %u\n", length);
-          hexdump(payload, length);
-          break;
+
+        JsonObject jsonObject = doc[1].as<JsonObject>();
+        String command = jsonObject["action"];
+
+        if (command == "feed") {
+          Serial.println("Command: Feed!");
+          
+          // feedAnimal();
+          trying++;
+        } else if (command == "set_interval") {
+          int interval = jsonObject["interval"];
+          feedingInterval = interval * 60000;
+        } else if (command == "get_frame") {
+          camera_fb_t *fb = esp_camera_fb_get();
+          if (!fb) {
+            Serial.println("Failed to capture image");
+            return;
+          }
+          sendImage(fb);
+          esp_camera_fb_return(fb);
+        } else {
+          Serial.println("Unknown command: " + command);
         }
-        case sIOtype_ERROR:
-            // Serial.println("[IOc] get error: %u\n", length);
-            hexdump(payload, length);
-            break;
-        case sIOtype_BINARY_EVENT:
-            // Serial.println("[IOc] get binary: %u\n", length);
-            hexdump(payload, length);
-            break;
-        case sIOtype_BINARY_ACK:
-            // Serial.println("[IOc] get binary ack: %u\n", length);
-            hexdump(payload, length);
-            break;
-    }
+        break;
+      }
+    default:
+      break;
+  }
 }
 
-
-void handleConfig(AsyncWebServerRequest *request) {
+void handleConfig(AsyncWebServerRequest* request) {
   if (request->hasParam("ssid") && request->hasParam("password")) {
     wifiSSID = request->getParam("ssid")->value();
     wifiPassword = request->getParam("password")->value();
     saveWiFiCredentials(wifiSSID, wifiPassword);
     WiFi.softAPdisconnect(true);
     WiFi.begin(wifiSSID.c_str(), wifiPassword.c_str());
-    request->send(200, "text/html; charset=utf-8", "<b>Устройство сейчас будет перезагруженно...<\b>");
-  }
-  else {
+    request->send(200, "text/html; charset=utf-8", "<b>Device will now reboot...</b>");
+  } else {
     request->send(200, "text/html; charset=utf-8", "<form action=\"/config\" method=\"get\">SSID:<input name=\"ssid\"><br>Password:<input name=\"password\" type=\"password\"><br><input type=\"submit\" value=\"Save\"></form>");
   }
 }
@@ -182,13 +215,17 @@ void handleConfig(AsyncWebServerRequest *request) {
 void setup() {
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-  servo.attach(servoPin);
-  servo.write(0);
+  feederServo.attach(servoPin);
+  feederServo.write(0);
+  // feederServo.detach();
+
+  Serial.begin(9600);
 
   loadWiFiCredentials(wifiSSID, wifiPassword);
   if (wifiSSID.length() > 0 && wifiPassword.length() > 0) {
     WiFi.begin(wifiSSID.c_str(), wifiPassword.c_str());
   }
+
   int attempts = 0;
   while (WiFi.status() != WL_CONNECTED && attempts < 20) {
     delay(500);
@@ -197,71 +234,82 @@ void setup() {
   }
 
   if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("\nПодключено!");
+    Serial.println("\nConnected to Wi-Fi!");
     Serial.println(WiFi.localIP());
   } else {
-    Serial.println("\nНе удалось подключиться к Wi-Fi.");
+    Serial.println("\nFailed to connect to Wi-Fi. Starting AP mode.");
+  }
+  WiFi.softAP(deviceID, password);
+  Serial.println("AP IP address: " + WiFi.softAPIP().toString());
+
+  server.on("/config", HTTP_GET, handleConfig);
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    String html = "<h1>AutoFeeder Setup</h1><a href=\"/config\">Configure Wi-Fi</a><br>";
+    request->send(200, "text/html; charset=utf-8", html);
+  });
+
+  server.begin();
+
+camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_QVGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_QQVGA;
+    config.jpeg_quality = 11;
+    config.fb_count = 1;
   }
 
-  if (attempts > 19) {
-    WiFi.disconnect(true);
-    delay(500);
-    WiFi.softAP(ssidAP, passwordAP);
-    delay(500);
+  if (esp_camera_init(&config) != ESP_OK) {
+    Serial.println("Ошибка инициализации камеры");
+    return;
   }
-  else {
-    // webSocket.beginSocketIO(serverHost, serverPort);
-    // webSocket.onEvent(webSocketEvent);
 
-    // socketIO.setReconnectInterval(10000);
+  Serial.println("Камера инициализирована");
+
+  if (WiFi.status() == WL_CONNECTED) {
     socketIO.begin(serverHost, serverPort, "/socket.io/?EIO=4");
     socketIO.onEvent(socketIOEvent);
   }
 
-  Serial.begin(115200);
-  Serial.println("Точка доступа запущена. Подключитесь к сети AutoFeederSetup.");
+  
 
-
-  server.on("/config", HTTP_GET, handleConfig);
-
-
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    String html = "<h1>Подключение кормушки</h1>";
-    html += "<a href=\"/config\">Настройки сети</a><br>";
-    request->send(200, "text/html; charset=utf-8", html);
-  });
-
-
-  server.begin();
 }
 
 
 void loop() {
-    // webSocket.loop();
-    if (trying > 1) {
-      test();
-      trying -= 1;
-    }
-    // feedAnimal();
-    socketIO.loop();
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastFeedingTime >= feedingInterval) {
+      lastFeedingTime = currentMillis;
 
-    //5 секунд
-    static unsigned long lastSendTime = 0;
-    if (millis() - lastSendTime > 5000) {
-        lastSendTime = millis();
-        if (socketIO.isConnected()) {
-          // DynamicJsonDocument doc(1024);
-          // JsonArray array = doc.to<JsonArray>();
-          // array.add("identify");
+      feedAnimal();
+  }
+  if (trying >= 1) {
+    feedAnimal();
+    trying--;
+  }
 
-          // JsonObject param1 = array.createNestedObject();
-          // JsonObject param2 = array.createNestedObject();
-          // param1["device_id"] = "feeder_001";
-          // param2["status"] = "ok";
-          // String output;
-          // serializeJson(doc, output);
-          // socketIO.sendEVENT(output);
-
-        }
-    }
+  socketIO.loop();
 }
